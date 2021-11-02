46d33f5eca6450c60bf9b4c2aee6af1951d7493a
Artur Campos <arturcamposba@gmail.com>
2021-11-02 17:46:29 -0300

    WIP

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -5,6 +5,8 @@ import CursosForm from './pages/cursos/CursosForm'
 import AlunosForm from './pages/alunos/AlunosForm'
 import ProfessoresForm from "./pages/professores/ProfessoresForm";
 import DisciplinasForm from "./pages/disciplinas/DisciplinasForm";
+import Curso from "./pages/cursos/Curso";
+import Alunos from "./pages/alunos/Alunos";
 
 
 const Rotas = () => {
@@ -12,11 +14,14 @@ const Rotas = () => {
     <>
       <Switch>
         <Container className="mt-3">
-          <Route exact path="/" component={CursosForm} />
-          <Route exact path="/alunos" component={AlunosForm} />
-          <Route exact path="/professores" component={ProfessoresForm} />
-          <Route exact path="/cursos" component={CursosForm} />
-          <Route exact path="/disciplinas" component={DisciplinasForm} />
+        <Route exact path="/" component={Curso} />
+                <Route exact path="/cursos" component={Curso} />
+                <Route exact path="/cursos/create" component={CursosForm} />
+                <Route exact path="/cursos/:id" component={CursosForm} />
+                <Route exact path="/alunos" component={Alunos} />
+                <Route exact path="/alunos/create" component={AlunosForm} />
+                <Route exact path="/alunos/:id" component={AlunosForm} />
+                <Route exact path="/professores" component={ProfessoresForm} />
         </Container>
       </Switch>
     </>
